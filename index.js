48b87c700b673b7cd8ffa0a20e9a03e56720692d
ajay <apsinghrana100@gamil.com>
2025-04-07 17:53:33 +0530

    adding some api

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("home page ");
   }
+  if (req.url === "/test") {
+    console.log("i am insdie")
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("X-Foo", "bar");
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("testing re-deploy ");
+  }
 });
 
 server.listen(5000, () => {
